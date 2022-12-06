10d26ec2934edc01f39d31d397b6e0020593cf45
etouraille <eouard.touraille@gmail.com>
2022-12-06 06:31:17 +0100

    [] remove mariadb files

--- component/google-signin/google-signin.component.ts
+++ component/google-signin/google-signin.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {GoogleLoginProvider, SocialAuthService} from "angularx-social-login";
 import {SubscribeComponent} from "../subscribe/subscribe.component";
 import {HttpClient} from "@angular/common/http";
@@ -7,6 +7,7 @@ import {StorageService} from "../../service/storage.service";
 import {Router} from "@angular/router";
 import {Store} from "@ngrx/store";
 import {login} from "../../actions/login-action";
+import {environment} from "../../../environments/environment";
 
 @Component({
   selector: 'app-google-signin',
@@ -26,14 +27,40 @@ export class GoogleSigninComponent extends SubscribeComponent implements OnInit 
     super();
   }
 
-  ngOnInit(): void {
+
+
+  auth2: any;
+
+  @ViewChild('loginRef', {static: true }) loginElement!: ElementRef;
+
+
+  /*------------------------------------------
+  --------------------------------------------
+  About
+  --------------------------------------------
+  --------------------------------------------*/
+  ngOnInit() {
+
+    this.googleAuthSDK();
   }
 
-  signin(): void {
-    this.socialAuthService.signIn(GoogleLoginProvider.PROVIDER_ID)
-      .then((data:any) => {
-        console.log(data);
-        this.add(this.http.post('google/signin', { token: data.idToken}).subscribe((data: any) => {
+  /**
+   * Write code on Method
+   *
+   * @return response()
+   */
+  callLoginButton() {
+
+    this.auth2.attachClickHandler(this.loginElement.nativeElement, {},
+      (googleAuthUser:any) => {
+
+        let profile = googleAuthUser.getBasicProfile();
+        console.log('Token || ' + googleAuthUser.getAuthResponse().id_token);
+        console.log('ID: ' + profile.getId());
+        console.log('Name: ' + profile.getName());
+        console.log('Image URL: ' + profile.getImageUrl());
+        console.log('Email: ' + profile.getEmail());
+        this.add(this.http.post('google/signin', { token: googleAuthUser.getAuthResponse().id_token}).subscribe((data: any) => {
           if(data.token) {
             this.storage.set('token', data.token);
             this.store.dispatch(login());
@@ -43,6 +70,41 @@ export class GoogleSigninComponent extends SubscribeComponent implements OnInit 
         }, (error: any) => {
           this.toastR.error('Echec de la connexion');
         }));
+
+          /* Write Your Code Here */
+
+      }, (error:any) => {
+        alert(JSON.stringify(error, undefined, 2));
       });
+
+  }
+
+  /**
+   * Write code on Method
+   *
+   * @return response()
+   */
+  googleAuthSDK() {
+
+    (<any>window)['googleSDKLoaded'] = () => {
+      (<any>window)['gapi'].load('auth2', () => {
+        this.auth2 = (<any>window)['gapi'].auth2.init({
+          client_id: environment.google,
+          cookiepolicy: 'single_host_origin',
+          scope: 'profile email'
+        });
+        this.callLoginButton();
+      });
+    }
+
+    (function(d, s, id){
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) {return;}
+      js = d.createElement('script');
+      js.id = id;
+      js.src = "https://apis.google.com/js/platform.js?onload=googleSDKLoaded";
+      fjs?.parentNode?.insertBefore(js, fjs);
+    }(document, 'script', 'google-jssdk'));
+
   }
 }
